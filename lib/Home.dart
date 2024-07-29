e3755590d7af946075b2dc68b9b8288473fab6f0
saliq03 <javidsaliq@gmail.com>
2024-07-29 20:05:03 +0530

    home screen designed

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,6 +11,71 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black26,
+      body: SafeArea(
+        child: Column(
+          children: [
+           const SizedBox(height: 20),
+           const PieChart(
+              dataMap:{
+              "Total":20,
+              "Death":15,
+              "Recovered":5
+            },
+            animationDuration: Duration(milliseconds: 1000),
+              chartType: ChartType.ring,
+              legendOptions: LegendOptions(
+                  legendPosition: LegendPosition.left,
+                legendTextStyle:TextStyle(color: Colors.white,fontWeight: FontWeight.bold),
+              ),
+              chartRadius: 150,
+
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Card(
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                        reusableRow("Total", "20"),
+                      reusableRow("Total", "20"),
+                      reusableRow("Total", "20"),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+         const SizedBox(height: 20,),
+         Container(
+              padding:const EdgeInsets.symmetric(vertical: 15),
+              margin:const EdgeInsets.symmetric(horizontal: 20),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(11)
+              ),
+              child:const Center(child: Text("Track Countries",style: TextStyle(fontWeight: FontWeight.bold),)),
+             
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  reusableRow(String title,String value){
+    return Column(
+      children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title),
+            Text(value)
+          ],
+        ),
+       const Divider(),
+      ],
+    );
   }
 }
