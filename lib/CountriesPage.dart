30a354a8aaee3a7080ebbcfb730ce121abacaf53
saliq03 <javidsaliq@gmail.com>
2024-07-31 13:13:17 +0530

    making search field functional

--- lib/CountriesPage.dart
+++ lib/CountriesPage.dart
@@ -26,6 +26,9 @@ class _CountriespageState extends State<Countriespage> {
               SizedBox(height: 30),
               TextField(
                 controller: searchController,
+                onChanged: (value){
+                  setState(() {});
+                },
                 style: TextStyle(color: Colors.white),
                 decoration: InputDecoration(
                   hintText: "Search country",
@@ -46,6 +49,7 @@ class _CountriespageState extends State<Countriespage> {
                   ),
                 ),
               ),
+              SizedBox(height: 15),
               FutureBuilder(
                   future: statesServices.CountriesData(),
                   builder: (context,AsyncSnapshot<List<CountriesJsonModel>> snapshot){
@@ -53,14 +57,43 @@ class _CountriespageState extends State<Countriespage> {
                       return Expanded(
                         child: ListView.builder(itemCount: snapshot.data!.length,
                             itemBuilder: (context,index){
-                          return ListTile(
-                            leading: Image(
-                              height: 50,
-                              width: 70,
-                              image: NetworkImage(snapshot.data![index].countryInfo!.flag!),
-                            ),
-                            title: Text(snapshot.data![index].country!,style: TextStyle(color: Colors.white,fontSize: 20),),
-                          );
+                          String name=snapshot.data![index].country!;
+                          if(searchController.text==''){
+                            return Column(
+                              children: [
+                                ListTile(
+                                  leading: Image(
+                                    height: 50,
+                                    width: 70,
+                                    image: NetworkImage(snapshot.data![index].countryInfo!.flag!),
+                                  ),
+                                  title: Text(name,style: TextStyle(color: Colors.white,fontSize: 20),),
+                                  subtitle:Text(snapshot.data![index].cases!.toString(),style: TextStyle(color: Colors.white),),
+                                ),
+                                SizedBox(height: 10,)
+                              ],
+                            );
+                          }
+                          else if(name.toLowerCase().contains(searchController.text.toLowerCase())){
+                            return Column(
+                              children: [
+                                ListTile(
+                                  leading: Image(
+                                    height: 50,
+                                    width: 70,
+                                    image: NetworkImage(snapshot.data![index].countryInfo!.flag!),
+                                  ),
+                                  title: Text(name,style: TextStyle(color: Colors.white,fontSize: 20),),
+                                  subtitle:Text(snapshot.data![index].cases!.toString(),style: TextStyle(color: Colors.white),),
+                                ),
+                                SizedBox(height: 10,)
+                              ],
+                            );
+                          }
+                          else{
+                            return Container(color: Colors.redAccent,);
+                          }
+
 
                         }),
                       );
